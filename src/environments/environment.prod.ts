b41b50ba24071cc0c4ac2188dc06c9901a6b80ff
Berk <berkelmas96@gmail.com>
2020-05-20 13:54:25 +0300

    https added.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: "http://bsv.bkbsoftapp.xyz/api/v1.0/",
-  apiMediaEndpoint: "http://localhost:8000/media/"
+  apiEndpoint: "https://bsv.bkbsoftapp.xyz/api/v1.0/",
+  apiMediaEndpoint: "http://localhost:8000/media/",
 };
